0218a1b1213bec2fa99f912bb1a8f60b4942ed84
Gareth Hutchins <Gareth.Hutchins@gmail.com>
2019-11-26 10:29:03 +0000

    Add project files.

---
+++ CaptivaAutoAnnotate/AutoAnnotate.cs
@@ -0,0 +1,37 @@
+﻿using Emc.InputAccel.ImageFilter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public sealed class AutoAnnotate : ImageFilter
+{
+    public override string Name
+    {
+        get { return "AutoAnnotateFilter"; }
+    }
+
+    public override string DisplayName
+    {
+        get { return "Auto Annotate Filter"; }
+    }
+
+    public override bool MultiThreadAccessAllowed
+    {
+        get
+        {
+            return false;
+        }
+    }
+
+    public override ImageData ApplyFilter(IFilterConfiguration configuration, ImageData imageData, Dictionary<string, object> outValues)
+    {
+        ImageData OutputImage = imageData;
+
+        return OutputImage;
+            
+    }
+}
+
---
+++ GetCoordinates/GetCoordinates.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom.InputAccel.UimScript
+{
+    using Emc.InputAccel.UimScript;
+    public class GetCoordinates : UimScriptMainCore
+    {
+
+    }
+}
