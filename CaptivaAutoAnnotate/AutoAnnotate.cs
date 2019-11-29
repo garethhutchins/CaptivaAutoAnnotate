d0cebad6b25b95f64f9ce0a3165b211708f3ece2
Gareth Hutchins <Gareth.Hutchins@gmail.com>
2019-11-29 16:50:31 +0000

    Added AutoAnnotate Filter.

--- CaptivaAutoAnnotate/AutoAnnotate.cs
+++ CaptivaAutoAnnotate/AutoAnnotate.cs
@@ -1,35 +1,47 @@
-﻿using Emc.InputAccel.ImageFilter;
-using Emc.InputAccel.UimScript;
-using Emc.InputAccel.CaptureClient;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Forms;
+﻿
 
 namespace Custom.InputAccel.UimFilterScript
 {
+   
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.IO;
+    using System.Windows;
+    using Emc.InputAccel.ImageFilter;
+    using Emc.InputAccel.UimScript;
     public class ScriptAutoAnnotate : UimScriptFilter
     {
         public ScriptAutoAnnotate()
 			: base()
 		{
 		}
-    public override ImageData ExecuteSuperFilter(ImageData imageData, string stepCustomValue, IUimScriptFilterContext uimScriptFilterContext, Dictionary<string, object> outValues)
+        /// <summary>
+        /// Executes custom super filter and extends output values 
+        /// </summary>
+        /// <param name="imageData">Image data with source image stream</param>
+        /// <param name="stepCustomValue">Custom script parameters.</param>
+        /// <param name="uimScriptFilterContext">Image super filter object</param>
+        /// <param name="outValues">Extra data's collection returned by the script</param>
+        /// <returns>Image data object that contains result image. Must be released on client side</returns>
+        public override ImageData ExecuteSuperFilter(ImageData imageData, string stepCustomValue, IUimScriptFilterContext uimScriptFilterContext, Dictionary<string, object> outValues)
         {
             //The filter to redact the selected area
-
+            //Create the dictionary if it doesn't exist
+            if (outValues == null)
+            {
+                outValues = new Dictionary<string, object>();
+            }
             //Create a new rectangle
             Rect rect = new Rect();
 
             //Set the annotation Type
             String AnnotationType = "Redact";
-            Int32 argbColourForegound = 1;
-            Int32 argbColourBackground = 1;
+
+            Int32 argbColourForegound = Color.Black.ToArgb();
+            Int32 argbColourBackground = Color.Black.ToArgb();
             Double opacity = 1.0;
-            String formtatText = "N/A";
+            String formatText = "NA";
             string[] arrSplit;
 
             //Check that there are some coordinates
@@ -43,8 +55,9 @@ namespace Custom.InputAccel.UimFilterScript
                 //y and height
                 rect.Y = Convert.ToDouble(arrSplit[2]);
                 rect.Height = (Convert.ToDouble(arrSplit[3]) - rect.Y);
+
                 //Now apply the filter
-                uimScriptFilterContext.CreateAndApplyAnnotation(rect, AnnotationType, argbColourForegound, argbColourBackground, opacity, formtatText);
+                uimScriptFilterContext.CreateAndApplyAnnotation(rect, AnnotationType, argbColourForegound, argbColourBackground, opacity, formatText);
             }
             //return the image
             return imageData;
